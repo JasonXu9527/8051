ef979bf4c03b816c28326386235d76cf278a0c33
JasonXu9527 <36103758+JasonXu9527@users.noreply.github.com>
2019-03-04 19:46:25 +0800

    Create port.h

---
+++ port.h
@@ -0,0 +1,21 @@
+#include <all.h>
+
+void port_init(void)
+{
+	P0M0 = 0X00;
+	P0M1 = 0X00;
+	P1M0 = 0X00;
+	P1M1 = 0X00;
+	P2M0 = 0X00;
+	P2M1 = 0X00;
+	P3M0 = 0X00;
+	P3M1 = 0X00;
+	P4M0 = 0X00;
+	P4M1 = 0X00;
+	P5M0 = 0X00;
+	P5M1 = 0X00;
+	P6M0 = 0X00;
+	P6M1 = 0X00;
+	P7M0 = 0X00;
+	P7M1 = 0X00;
+}
