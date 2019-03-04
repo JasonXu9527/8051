19b37a53fa5f113ebce341b9f36583adb22c38b4
JasonXu9527 <36103758+JasonXu9527@users.noreply.github.com>
2019-03-04 19:49:59 +0800

    Create led.h

---
+++ led.h
@@ -0,0 +1,8 @@
+#ifndef __LED_H__
+#define __LED_H__
+
+extern void led_on(unsigned char dis_dat);
+extern void led_off(unsigned char dis_dat);
+extern void led_swap(unsigned char dis_dat);
+
+#endif
