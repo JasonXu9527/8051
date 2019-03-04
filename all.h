d7a96c4a04cc7b8d388cb1af4dec68a898ce11d0
JasonXu9527 <36103758+JasonXu9527@users.noreply.github.com>
2019-03-04 19:45:00 +0800

    Create all.h

---
+++ all.h
@@ -0,0 +1,22 @@
+#ifndef __ALL_H__
+#define __ALL_H__
+
+#include <STC15F2K60S2.h>
+#include <intrins.h>
+#include <port.h>
+#include <time.h>
+#include <led.h>
+#include <led8.h>
+#include <key.h>
+//#include <keyboard.h>
+#include <uart.h>
+//#include <onewire.h>
+#include <iic.h>
+#include <ds1302.h>
+#include <iicbus.h>
+#include <control.h>
+
+extern bit f_20ms;
+
+
+#endif
