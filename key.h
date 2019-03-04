1c46dc344e10e55b05f56d4875da5d85a039da8b
JasonXu9527 <36103758+JasonXu9527@users.noreply.github.com>
2019-03-04 19:51:47 +0800

    Create key.h

---
+++ key.h
@@ -0,0 +1,15 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+
+extern void key_read_mainloop(void);
+extern unsigned char key_code;
+extern void key_deal_mainloop(void);
+
+extern unsigned char run_state;
+extern unsigned char fmq_warning;
+
+extern unsigned char rh_set;
+extern unsigned char rh_dat;
+
+
+#endif
